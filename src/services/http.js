47d66d94c6364214f966cd7642f0aeaa3a418ffb
mitchpk <kphctim@gmail.com>
2022-10-05 21:23:44 +1030

    show proper error for refresh failure

--- src/services/http.js
+++ src/services/http.js
@@ -26,6 +26,7 @@ http.interceptors.response.use(
         await axios.get("/api/refresh");
         return http(originalRequest);
       } catch (err) {
+        error = err;
         store.commit("auth/setLoggedIn", false);
         router.push("/");
       }
