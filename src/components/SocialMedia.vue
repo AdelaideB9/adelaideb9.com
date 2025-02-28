bf7c5308b04f0c99a9d714a78790cae31573d353
doggodile <hello@adelaideb9.com>
2025-03-01 01:48:54 +1030

    Add Instagram

--- src/components/SocialMedia.vue
+++ src/components/SocialMedia.vue
@@ -2,13 +2,14 @@
 defineProps({
   href: String(),
   src: String(),
+  style: String(),
   exclusive: Boolean,
 });
 </script>
 
 <template>
   <a class="no-underline text-xs" target="_blank" :href="href">
-    <div v-if="exclusive" class="flex bg-soft-purple/50 rounded-md p-2 gap-2">
+    <div v-if="exclusive" class="flex bg-soft-purple/50 rounded-md p-2 gap-2" :style="style">
       <img :src="src" class="h-8" />
       <p class="hidden md:block font-medium tracking-tighter text-center">
         Members<br />Exclusive
